49814eceda1f546207c7f53e5699959578c91d95
Amol <amolborkar5@gmail.com>
2019-10-08 00:15:46 +0530

    Added more columns in file explorer.

--- src/components/FileExplorer.vue
+++ src/components/FileExplorer.vue
@@ -2,7 +2,7 @@
   <div class="file-explorer">
     <span class="subtitle">
       Files in Openload Server
-      <button class="button is-rounded" @click="fetchFiles">
+      <button class="button is-small" @click="fetchFiles">
         <span class="icon is-small">
           <i class="material-icons">refresh</i>
         </span>
@@ -14,13 +14,17 @@
           <th>No.</th>
           <th>File Name</th>
           <th>Size (Mb)</th>
+          <th>Uploaded At</th>
+          <th>Downloads</th>
         </tr>
       </thead>
       <tbody v-for="(file, i) in files" :key="file.name">
         <tr>
           <th>{{ i + 1}}</th>
-          <td>{{ file.name }}</td>
-          <td>{{ file.size / (1024 * 1024) }}</td>
+          <td><a :href="file.link" target="_blank">{{ file.name }}</a></td>
+          <td>{{ (file.size / (1024 * 1024)).toString().substring(0, 8) }}</td>
+          <td>{{ new Date(parseInt(file.upload_at)).toLocaleDateString() }}</td>
+          <td class="has-text-centered">{{ file.download_count }}</td>
         </tr>
       </tbody>
     </table>
