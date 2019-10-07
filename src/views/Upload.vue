ff9dcfe042f9faaa08d5cf12707d2432eafa9395
Amol <amolborkar5@gmail.com>
2019-10-07 12:57:12 +0530

    Created new route and view for media uploads.

---
+++ src/views/Upload.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="upload">
+    <UploadFile />
+  </div>
+</template>
+
+<script>
+import UploadFile from '@/components/UploadFile.vue'
+
+export default {
+  name: 'upload',
+  components: {
+    UploadFile
+  }
+}
+</script>
