046c8b656a9541154e82cf6b83663b27b3b401c0
Amol <amolborkar5@gmail.com>
2019-10-07 11:48:24 +0530

    Removed account info component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <UploadFile />
-    <AccountInfo />
   </div>
 </template>
 
 <script>
-import AccountInfo from '@/components/AccountInfo.vue'
 import UploadFile from '@/components/UploadFile.vue'
 
 export default {
   name: 'home',
   components: {
-    AccountInfo,
     UploadFile
   }
 }
