02ef4308080d34ef28f115ffca3ab84407c46e21
Amol <amolborkar5@gmail.com>
2019-10-10 00:02:35 +0530

    Changed notification position to top left.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <notifications group="all" position="top center"/>
+    <notifications group="all" position="top left"/>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/upload">Upload</router-link> |
