7dfa5f480cbaaf3224b68dc4c3a99ebede54c160
Amol <amolborkar5@gmail.com>
2019-10-10 00:42:03 +0530

    Added a proper header.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
     <notifications group="all" position="top left"/>
-    <div id="nav">
+    <!--<div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/upload">Upload</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
+    <Header />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar is-warning is-small" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <router-link class="navbar-item" to="/">
+      <h1 class="title">Radio</h1>
+    </router-link>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-end">
+      <router-link class="navbar-item" to="/">
+        Home
+      </router-link>
+
+      <router-link class="navbar-item" to="/upload">
+        Upload
+      </router-link>
+
+      <router-link class="navbar-item" to="/about">
+        About
+      </router-link>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style>
+
+</style>
